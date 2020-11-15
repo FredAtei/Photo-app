9aad5e8000422b9c1769b2559d0b4b9dfb516718
Fred <akamaatei@gmail.com>
2020-11-15 18:03:30 +0300

    add models tests

--- pictures/models.py
+++ pictures/models.py
@@ -9,11 +9,41 @@ class Image(models.Model):
     image_name = models.CharField(max_length =30)
     image_description = models.TextField()
     image_location = models.ForeignKey('Location',on_delete=models.CASCADE)
-    image_category = models.ForeignKey('Category',on_delete=models.CASCADE)
+    image_category = models.ForeignKey('Category',on_delete=models.CASCADE) 
+
+    def __str__(self):
+        return self.image_name
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def get_images(cls):
+        all_images = cls.objects.all()
+        return all_images
+
+    @classmethod
+    def filter_by_location(cls,id):
+        image_by_location = cls.objects.filter(image_location_id = id)  
+        return image_by_location
+
+    @classmethod
+    def search_image(cls,category):
+        photos = cls.objects.filter(image_category__name__icontains=category) 
+        return photos
 
 class Location(models.Model):
     name = models.CharField(max_length = 30)
 
+    def save_location(self):
+        self.save()
+
 class Category(models.Model):
     name = models.CharField(max_length = 30)
+
+    def save_category(self):
+        self.save()
 
--- pictures/tests.py
+++ pictures/tests.py
@@ -1,3 +1,52 @@
 from django.test import TestCase
+from .models import Image,Location,Category
 
 # Create your tests here.
+class CategoryTestClass(TestCase):
+    def setUp(self):
+        self.travel = Category(name='travel')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.travel,Category)) 
+
+    def test_save_method(self):
+        self.travel.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories)>0)
+
+class LocationTestClass(TestCase):
+    def setUp(self):
+        self.Paris = Location(name='Paris')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Paris,Location)) 
+
+    def test_save_method(self):
+        self.Paris.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations)>0)       
+
+class ImageTestClass(TestCase):
+    def setUp(self):
+        self.travel = Category(name = 'travel')
+        self.travel.save_category()
+
+        self.locations = Location(name='Paris')
+        self.locations.save_location()
+
+        self.new_image=Image(image_name='Eot',image_description='Great things',image_category=self.travel,image_location=self.locations)
+        self.new_image.save_image()
+
+    def tearDown(self):
+        Category.objects.all().delete()
+        Location.objects.all().delete()
+        Image.objects.all().delete()
+
+    def test_get_images(self):
+        all_images = Image.get_images()
+        self.assertTrue(len(all_images)>0)
+
+    def test_filter_by_location(self):
+        test_location_id = 6
+        images_location = Image.filter_by_location(test_location_id) 
+        self.assertTrue(len(images_location) == 0)              
