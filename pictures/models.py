3eb7982c135634e188e73b7c945429b534c51ecd
Fred <akamaatei@gmail.com>
2020-11-15 17:01:31 +0300

    add model and new features

--- pictures/models.py
+++ pictures/models.py
@@ -2,5 +2,10 @@ from django.db import models
 from cloudinary.models import CloudinaryField
 
 class Picture(models.Model):
-  image = CloudinaryField('image')
+    image = CloudinaryField('image')
+
+class Image(models.Model):
+    image=models.ImageField(upload_to = 'photos/')
+    image_name=models.CharField(max_length =60)
+    image_description=models.TextField()  
 
